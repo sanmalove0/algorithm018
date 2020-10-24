4b957a3f8fb2f77e0c7158cad321ba9e8d147033
sanma <1204425892@qq.com>
2020-10-25 01:26:39 +0800

    week01

--- week01/RewriteDeque.java
+++ week01/RewriteDeque.java
@@ -6,21 +6,21 @@ public class RewriteDeque {
         // 示例代码 - Deque
         /*
         Deque<String> deque = new LinkedList<String>();
-        deque.addFirst("a");
-        deque.addFirst("b");
-        deque.addFirst("c");
+        deque.push("a");
+        deque.push("b");
+        deque.push("c");
         System.out.println(deque);
 
-        String str = deque.peekFirst();
+        String str = deque.peek();
         System.out.println(str);
         System.out.println(deque);
 
         while(deque.size() > 0) {
-            System.out.println(deque.removeFirst());
+            System.out.println(deque.pop());
         }
         System.out.println(deque);
         */
-
+        
         // 用新的API改写后
         Deque<String> deque = new LinkedList<String>();
         deque.addFirst("a");
